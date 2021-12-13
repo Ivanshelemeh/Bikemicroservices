1d1127348bac915a90a464978f82026ad2018b48
Ivanshelemeh <neo041990@mail.ru>
2021-12-13 09:39:02 +0300

    add gitignore

---
+++ src/test/java/com/example/bikecustomservise/api/BikeCustomServiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bikecustomservise.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeCustomServiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
