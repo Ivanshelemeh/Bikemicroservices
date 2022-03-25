72f08b26d6906f1024705382c93c58a78e58afa4
Ivanshelemeh <neo041990@mail.ru>
2022-03-25 19:12:24 +0300

    added spring cloud bus for changing properties though git

--- src/main/java/com/example/bikecustomservise/api/rest/BikeCustomerLoginController.java
+++ src/main/java/com/example/bikecustomservise/api/rest/BikeCustomerLoginController.java
@@ -3,7 +3,6 @@ package com.example.bikecustomservise.api.rest;
 import com.example.bikecustomservise.api.dto.BikeCustomerRequestModel;
 import com.example.bikecustomservise.api.dto.BikeCustomerResponseModel;
 import com.example.bikecustomservise.api.dto.BikeCustomerSharedDTO;
-import com.example.bikecustomservise.api.service.BikeCustomerServiceImpl;
 import com.example.bikecustomservise.api.service.BikeLogInServiceImpl;
 import com.example.bikecustomservise.api.utilit.BikeCustomerMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -33,7 +31,7 @@ public class BikeCustomerLoginController {
     @RequestMapping(value = "/login"
             , produces = MediaType.APPLICATION_JSON_VALUE, method = {RequestMethod.GET, RequestMethod.POST})
     public String login() {
-        return "Work in port" + env.getProperty("server.port");
+        return "Work in port" + env.getProperty("server.port") + "," + env.getProperty("token.secret");
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
